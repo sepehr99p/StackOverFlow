db45607f96e44c7442d3736ca0049a142dd9b3e0
Sep <sepehr.pourekrami@gmail.com>
2025-03-17 15:03:36 +0330

    loops cont

--- learning/loop.go
+++ learning/loop.go
@@ -18,4 +18,38 @@ func loops() {
 	for i < 3 {
 		i += 2
 	}
+
+	// Here rvariable is a array
+	rvariable := []string{"GFG", "Geeks", "GeeksforGeeks"}
+
+	// i and j stores the value of rvariable
+	// i store index number of individual string and
+	// j store individual string of the given array
+	for i, j := range rvariable {
+		fmt.Println(i, j)
+	}
+
+	// using maps
+	myMap := map[int]string{
+		22: "Geeks",
+		33: "GFG",
+		44: "GeeksforGeeks",
+	}
+	for key, value := range myMap {
+		fmt.Println(key, value)
+	}
+
+	// using channel
+	myChannel := make(chan int)
+	go func() {
+		myChannel <- 100
+		myChannel <- 1000
+		myChannel <- 10000
+		myChannel <- 100000
+		close(myChannel)
+	}()
+	for i := range myChannel {
+		fmt.Println(i)
+	}
+
 }
