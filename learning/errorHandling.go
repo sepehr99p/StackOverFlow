f5a8eab39020e75d70b5da084fd23f8c0e7b7076
Sep <sepehr.pourekrami@gmail.com>
2025-03-18 21:37:10 +0330

    error handling

---
+++ learning/errorHandling.go
@@ -0,0 +1,16 @@
+package learning
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	_, err := os.Open("no-file.txt")
+	if err != nil {
+		//		fmt.Println("err happened", err)
+		//		log.Println("err happened", err)
+		log.Fatalln(err)
+		//		panic(err)
+	}
+}
