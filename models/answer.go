2f26063885f87a33f627e8599b1d7e686c727a5f
Sep <sepehr.pourekrami@gmail.com>
2025-03-31 19:22:26 +0330

    minor update on answer & question pointer to comment

--- models/answer.go
+++ models/answer.go
@@ -1,13 +1,13 @@
 package models
 
 type Answer struct {
-	AnswerId    int       `json:"answer_id" gorm:"primary_key"`
-	QuestionId  int64     `json:"question_id" gorm:"index"` // Foreign key to Question
-	UserId      int64     `json:"user_id"`
-	DateCreated int64     `json:"date" gorm:"autoCreateTime"`
-	Description string    `json:"description"`
-	Votes       int       `json:"votes"`
-	Comments    []Comment `json:"comments" gorm:"foreignKey:ParentId;constraint:OnDelete:CASCADE;"`
+	AnswerId    int        `json:"answer_id" gorm:"primary_key"`
+	QuestionId  int64      `json:"question_id" gorm:"index"` // Foreign key to Question
+	UserId      int64      `json:"user_id"`
+	DateCreated int64      `json:"date" gorm:"autoCreateTime"`
+	Description string     `json:"description"`
+	Votes       int        `json:"votes"`
+	Comments    []*Comment `json:"comments" gorm:"foreignKey:ParentId;constraint:OnDelete:CASCADE;"`
 }
 
 var mockAnswer = Answer{UserId: 234, Description: "some answer description", Votes: 44, DateCreated: 33344}
--- models/question.go
+++ models/question.go
@@ -1,14 +1,14 @@
 package models
 
 type Question struct {
-	QuestionId  int64     `json:"question_id" gorm:"primary_key"`
-	UserId      int       `json:"user_id" gorm:"index"` // Foreign key to User
-	TagId       int       `json:"tag_idz"`
-	Description string    `json:"description"`
-	Votes       int       `json:"votes"`
-	DateCreated int64     `json:"date" gorm:"autoCreateTime"`
-	Answers     []*Answer `json:"answers" gorm:"many2many:question_answers;foreignKey:QuestionId;joinForeignKey:QuestionId"`
-	Comments    []Comment `json:"comments" gorm:"foreignKey:ParentId;constraint:OnDelete:CASCADE;"`
+	QuestionId  int64      `json:"question_id" gorm:"primary_key"`
+	UserId      int        `json:"user_id" gorm:"index"` // Foreign key to User
+	TagId       int        `json:"tag_idz"`
+	Description string     `json:"description"`
+	Votes       int        `json:"votes"`
+	DateCreated int64      `json:"date" gorm:"autoCreateTime"`
+	Answers     []*Answer  `json:"answers" gorm:"many2many:question_answers;foreignKey:QuestionId;joinForeignKey:QuestionId"`
+	Comments    []*Comment `json:"comments" gorm:"foreignKey:ParentId;constraint:OnDelete:CASCADE;"`
 }
 
 var mockQuestion = Question{
