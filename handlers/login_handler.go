88110ed8ec1115c544346c1ea0577be13c776cb7
Sep <sepehr.pourekrami@gmail.com>
2025-04-07 13:05:14 +0330

    register & login user updated & hashing impl

--- handlers/login_handler.go
+++ handlers/login_handler.go
@@ -5,6 +5,7 @@ import (
 	"Learning/models"
 	"Learning/token"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 	"net/http"
 	"strings"
 )
@@ -17,61 +18,85 @@ import (
 // @Success 201 {object} string
 // @Router /register [post]
 func RegisterHandler(c *gin.Context) {
-	var user models.UserRegister
-	if err := c.ShouldBindJSON(&user); err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid JSON format", "error": err.Error()})
-		return
-	}
-	var userModel = models.User{UserName: user.PhoneNumber, Password: user.Password}
-	dataExistenceResult := database.DB.Find(&userModel)
-	if dataExistenceResult.Error != nil {
-		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "error querying database"})
-		return
-	}
-	if dataExistenceResult.RowsAffected == 0 {
-		result := database.DB.Create(&user)
-		if result.Error != nil {
-			c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Error creating user", "error": result.Error.Error()})
-			return
-		}
-		tokenString, err := token.CreateToken(userModel.UserName)
-		if err != nil {
-			c.IndentedJSON(http.StatusUnauthorized, gin.H{"message": "Invalid credentials"})
-			return
-		}
-		c.IndentedJSON(http.StatusCreated, tokenString)
-	} else {
-		c.IndentedJSON(http.StatusConflict, gin.H{"message": "user already exist"})
+	var userInput models.UserRegister
+	if err := c.ShouldBindJSON(&userInput); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid JSON format", "error": err.Error(),
+		})
 		return
 	}
 
+	var count int64
+	database.DB.Model(&models.User{}).
+		Where("user_name = ?", userInput.PhoneNumber).
+		Count(&count)
+
+	if count > 0 {
+		c.IndentedJSON(http.StatusConflict, gin.H{"message": "User already exists"})
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(userInput.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Error hashing password"})
+		return
+	}
+
+	newUser := models.User{
+		UserName: userInput.PhoneNumber,
+		Password: string(hashedPassword),
+	}
+	result := database.DB.Create(&newUser)
+	if result.Error != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{
+			"message": "Error creating user", "error": result.Error.Error(),
+		})
+		return
+	}
+
+	tokenString, err := token.CreateToken(newUser.UserName)
+	if err != nil {
+		c.IndentedJSON(http.StatusUnauthorized, gin.H{"message": "Invalid credentials"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusCreated, tokenString)
 }
 
 // LoginHandler
 // @Tags auth
 // @Accept json
 // @Produce json
-// @Param answer body models.User true "User object"
-// @Success 201 {object} models.User
+// @Param answer body models.UserRegister true "User object"
+// @Success 201 {object} models.string
 // @Router /login [post]
 func LoginHandler(c *gin.Context) {
+
 	c.Header("Content-Type", "application/json")
-	var user models.User
+	var user models.UserRegister
 	if err := c.ShouldBindJSON(&user); err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid JSON format"})
 		return
 	}
-	dbUser := models.User{UserName: user.UserName, Password: user.Password}
+
+	dbUser := models.User{UserName: user.PhoneNumber, Password: user.Password}
 	if queryResult := database.DB.Find(&dbUser).Error; queryResult != nil {
 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
 		return
 	}
 
-	tokenString, err := token.CreateToken(user.UserName)
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(dbUser.Password))
 	if err != nil {
 		c.IndentedJSON(http.StatusUnauthorized, gin.H{"message": "Invalid credentials"})
 		return
 	}
+
+	tokenString, err := token.CreateToken(user.PhoneNumber)
+	if err != nil {
+		c.IndentedJSON(http.StatusUnauthorized, gin.H{"message": "Invalid credentials"})
+		return
+	}
+
 	c.IndentedJSON(http.StatusOK, tokenString)
 	return
 }
