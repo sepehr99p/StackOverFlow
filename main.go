02b21fc9b826755d224b88e05f76a5507f3111f7
Sep <sepehr.pourekrami@gmail.com>
2025-04-10 16:38:07 +0330

    move swagger endpoint to routes

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"Learning/database"
 	_ "Learning/docs"
 	routes "Learning/routers"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
 	"log"
 )
 
@@ -18,12 +16,9 @@ func main() {
 	<-ready
 
 	router := routes.SetupRouter()
-
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, ginSwagger.DefaultModelsExpandDepth(2)))
-
-	log.Println("Server running on localhost:8080")
-	err := router.Run("0.0.0.0:8080")
-	if err != nil {
+	if err := router.Run("0.0.0.0:8080"); err != nil {
 		log.Fatal("Failed to start server:", err)
+	} else {
+		log.Println("Server running on localhost:8080")
 	}
 }
